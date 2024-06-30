6300e8af47b6f9cd2cb9e26ab641036bae0e1c30
YOCELYN12 <yocelynrivera9@gmail.com>
2024-06-30 15:50:31 -0600

    Delete

--- src/Fetch/Fetch.js
+++ src/Fetch/Fetch.js
@@ -15,6 +15,31 @@ const Get = async()=>{
 export {Get}
 
 //Delete
+const Delete = async (id)=>{
+    try {
+        const eliminar = await fetch(URL,id, {
+            method: "DELETE",
+        headers: {
+            "Content-Type": "application/json"
+        }
+        })
+        const salida = await eliminar.json()
+
+        if (eliminar.ok){
+            alert("Se elimino exitosamente")
+            await Get()
+        }else {
+            alert("Eror")
+        }
+        console.log(salida)
+
+    } catch (error) {
+        console.log("error")
+    }
+}
+
+export {Delete}
+
 //Post
 //Put
 
