759e8428b20139e14fd2972c18bfefcc745f8ae9
YOCELYN12 <yocelynrivera9@gmail.com>
2024-07-09 14:55:33 -0600

    PutV2

--- src/Componentes/FormularioEditar.jsx
+++ src/Componentes/FormularioEditar.jsx
@@ -14,7 +14,34 @@ const FormularioEditar = () => {
 
     const validarPut = async () => {
 
-       
+        let actualizarDatos = {
+            producto: producto,
+            precio: precio,
+            marca: marca,
+            descripcion: descripcion,
+            unidades: unidades
+        }
+        await Put(id, estado, "productos")
+    }
+
+   
+
+    return (
+        <div>
+
+            <div className='contenedorEditar'>
+
+                <input className='inpEditar' type="text" onChange={(e) => setProducto(e.target.value)} placeholder='producto' />
+                <input className='inpEditar' type="number" onChange={(e) => setPrecio(e.target.value)} placeholder='precio' />
+                <input className='inpEditar' type="text" onChange={(e) => setMarca(e.target.value)} placeholder='marca' />
+                <input className='inpEditar' type="text" onChange={(e) => setDescripcion(e.target.value)} placeholder='descripcion' />
+                <input className='inpEditar' type="number" onChange={(e) => setUnidades(e.target.value)} placeholder='unidades' />
+
+            </div>
+
+            <button onClick={enviarDatos}>Editar Producto</button>
+        </div>
+    )
 }
 
 export default FormularioEditar
